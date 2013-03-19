db51cb0f3c172a4e854b1fb67f0efcef5f045e22
Tomas Martinez <timartin@uc.cl>
2013-03-19 19:41:52 -0400

    Aplicacion Basica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megaupload::Application.config.secret_token = '23c3b5429a9097b8d46ed592889990068767ef0afc5d07d60e2b6593f70a48690f79f2e1dbff1df92861be57191bf794f9b3e875dd5a2aba354b3fa344346a03'
