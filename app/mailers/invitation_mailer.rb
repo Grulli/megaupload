66e02eef964592d576e198f6bd75e34fd47bbf0e
Tomas Martinez <timartin@uc.cl>
2013-03-26 15:59:02 -0400

    arreglo de mails

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,7 +4,7 @@ class InvitationMailer < ActionMailer::Base
 	def invitation_mail(up_file, event)
 		@up_file = up_file
 		@event = event
-		@url = upload_path #+ "?mail=#up_file.mail&event=#event.id&token=up_file.gen_token()"
-		mail(:to => up_file.mail, :subject => "You have been invited to ")# + event.name)
+		@url = upload_path 
+		mail(:to => up_file.mail, :subject => "You have been invited to ")
 	end
 end
